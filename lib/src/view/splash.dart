472fe445069b5111ad99f454cc0514bdee6a4004
Eduard <eduardgrigoryan1993@gmail.com>
2021-09-25 16:59:32 +0400

    add logo in spash page

--- lib/src/view/splash.dart
+++ lib/src/view/splash.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:weather/src/widgets/widgets.dart' show Logo;
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Logo', style: TextStyle(fontSize: 18))),
-    );
+    return const Scaffold(body: Center(child: Logo()));
   }
 }

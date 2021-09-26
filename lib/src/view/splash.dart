61155f2732eafe8faad18a46cf385cdaa7fd3731
Eduard <eduardgrigoryan1993@gmail.com>
2021-09-26 13:09:57 +0400

    change logo size

--- lib/src/view/splash.dart
+++ lib/src/view/splash.dart
@@ -6,6 +6,7 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Logo()));
+    return const Scaffold(
+        body: Center(child: SizedBox(height: 150, child: Logo())));
   }
 }

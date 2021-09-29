7c8841d720520dab6b004621527a816f4adbac0e
Eduard <eduardgrigoryan1993@gmail.com>
2021-09-29 12:02:05 +0400

    config localization

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:weather/src/models/system.dart';
+import 'package:weather/src/shared/setup.dart';
 import 'package:weather/src/view/main.dart';
 import 'package:easy_localization/easy_localization.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
+  void setUp(BuildContext context) {
+    getIt<System>().lang = context.locale.languageCode;
+  }
+
   @override
   Widget build(BuildContext context) {
+    setUp(context);
     return MaterialApp(
       title: 'Weather',
       darkTheme: ThemeData(
--- lib/src/view/main.dart
+++ lib/src/view/main.dart
@@ -1,10 +1,8 @@
 import 'package:dio/dio.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather/src/bloc/base/base_bloc.dart';
-import 'package:weather/src/models/coord.dart';
-import 'package:weather/src/models/models.dart';
+import 'package:weather/src/models/models.dart' show System, Coord;
 import 'package:weather/src/repository/forecast.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:weather/src/shared/setup.dart';
